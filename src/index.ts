6ce2d24946e1bee3b1bc065d6caec91e1852fbf2
Technote <technote.space@gmail.com>
2022-06-23 03:06:13 +0900

    chore: tweaks

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
+import * as Constant from './constant.js';
 import * as Commit from './utils/commit.js';
 import * as Misc from './utils/misc.js';
 import * as Version from './utils/version.js';
 
-export { Commit, Version, Misc };
+export { Commit, Version, Misc, Constant };
